b421de780054e3e10268cbd728b182fb8d0d1ebb
Cenk <muzzyathome@gmail.com>
2022-04-28 20:28:48 +0100

    mocked api call

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const server = setupServer(
+  rest.get("https://ghibliapi.herokuapp.com/films", (req, res, ctx) => {
+    return res(
+      ctx.json([
+                {
+                  "id": "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+                  "title": "Castle in the Sky"
+                },
+                {
+                  "id": "12cfb892-aac0-4c5b-94af-521852e46d6a",
+                  "title": "Grave of the Fireflies"
+                }
+            ])
+        );
+    })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+
+
+
 
 test('renders Castle in the Sky', async () => {
   render(<App />);
